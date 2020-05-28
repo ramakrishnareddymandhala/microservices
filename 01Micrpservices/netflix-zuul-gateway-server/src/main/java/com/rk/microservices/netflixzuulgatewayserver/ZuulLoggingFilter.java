fe9666079fc878e061af91b8d098d00f599152ba
Rama Krishna Mandhala <ramakrishnareddymandhala@gmail.com>
2020-05-28 19:03:35 +0200

    Added Gateway
    Microservices can access through gateway

---
+++ 01Micrpservices/netflix-zuul-gateway-server/src/main/java/com/rk/microservices/netflixzuulgatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,39 @@
+package com.rk.microservices.netflixzuulgatewayserver;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest httpsReq = RequestContext.getCurrentContext().getRequest();
+		logger.info("request -> {} reuest uri -> {}",httpsReq,httpsReq.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
