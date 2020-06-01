4b370a51ba3d1bc69bab453d5001a4894550a7f4
Rama Krishna Mandhala <ramakrishnareddymandhala@gmail.com>
2020-06-01 20:08:17 +0200

    Final version of microservices
    Implemented Central Tracing using RabbitMq and Zipkin. 
    Install RabbitMq and run zipkin jar with RabbitMq server url.
    Commands : set RABBIT_MQ=amqp://localhost
    java -jar zipkin-server-2.21.2-exec.jar

--- 01Micrpservices/currency-conversion-service/src/main/java/com/rk/microservices/currencyconversionservice/CurrencyConversionController.java
+++ 01Micrpservices/currency-conversion-service/src/main/java/com/rk/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -2,6 +2,8 @@ package com.rk.microservices.currencyconversionservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyConversionController {
-
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private CurrencyExchangeServiceProxy currencyExchangeProxy;
 
@@ -18,6 +20,7 @@ public class CurrencyConversionController {
 			@PathVariable String to, @PathVariable BigDecimal quantity) {
 	
 		CurrencyConversionBean res =  currencyExchangeProxy.retrieveExchangeValue(from, to);
+		logger.info("In Conversion service "+from );
 		return new CurrencyConversionBean(1L,from,to,res.getConversionMultiple(),quantity,
 				quantity.multiply(res.getConversionMultiple()),res.getPort());
 	}
--- 01Micrpservices/currency-exchange-service/src/main/java/com/rk/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ 01Micrpservices/currency-exchange-service/src/main/java/com/rk/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,4 +1,6 @@
 package com.rk.microservices.currencyexchangeservice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
-	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private Environment environment;
 	
@@ -23,7 +25,7 @@ public class CurrencyExchangeController {
 		
 		exchangeValue.setPort(
 				Integer.parseInt(environment.getProperty("local.server.port")));
-		
+		logger.info("In Exchange server "+from );
 		return exchangeValue;
 	}
 }
